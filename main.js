4729b1088442a0499dc4fcc3c248e0208762c451
ArxelV <landon.holladay@stu.ocsb.ca>
2024-10-31 11:15:55 -0400

    commit 1

--- main.js
+++ main.js
@@ -27,3 +27,47 @@ function round(value, decimals) {
 function randInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+function startGame () {
+let userName = prompt("Whats your name?")
+alert(`Hello ${userName}! Welcome to your Adventure!`)
+
+let room_1 = prompt("Will you enter the cold throne of the Lich King OR the Elden Beasts eternal domain? *type 1 or 2* ")
+
+if (room_1 == 1) {
+    lich_king()
+}
+ if (room_1 == 2) {
+    elden_beast()
+ }
+
+}
+function lich_king() {
+
+        alert(`As you walk into the cold and icy throne, you look up to be completely frozen by the mear presence of the Lich King... He stares at you. He swings his Eternal Rune Long Sword, slicing you in half. Forever to be damned to the Shadow realm.`)
+    
+}
+
+function elden_beast() {
+ 
+    alert(`you are met with an eternal GOD. The Elden Beast. Forever powerful, ruling the Lands in between. A tarnished of your kind is a worthy opponent. It notices your strength. You are granted a choice.`)
+    let choice_eb = prompt(`${userName}... Do you stand strong and fight or do you cower in the face of an eternal GOD?  *type 1* or 2`)
+
+    if (choice_eb == 1) {
+        fight()
+    }
+     if (choice_eb == 2) {
+        cower()
+     }
+}
+
+
+function fight() {
+alert(`
+     The Elden Beast is impressed. But you are no Match. You are tattered and beaten to the point of unconciousness, but your will stands STRONG. You die standing, a true tarnished warrior indeed. The Ring wasnt meant for you after all
+     `)
+}
+
+function cower() {
+alert(` You run away in pure fear, the Elden beast was dissapointed. The Elden beast swung its eternal sword of Light and incinertates you, making all your efforts and struggles for nothing. Be fr bro, all those bosses and u gave up? smh. You get no candy.`)
+}
